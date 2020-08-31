67a6a6aa778ce5e939fc585cd1cbd60c9ad09a3b
Rodrigo <rodsiqueirabh@hotmail.com>
2020-08-31 12:28:37 -0300

    Criação do projeto e teste com HelloWorld

---
+++ src/main/java/br/com/rodrigorosa/linguicasiqueira/LinguicasiqueiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigorosa.linguicasiqueira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguicasiqueiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguicasiqueiraApplication.class, args);
+	}
+
+}
